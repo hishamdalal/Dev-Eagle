4160c52d17bc100dce9fcad003e0978e28a10b48
Hisham Dalal <hishamdalal@gmail.com>
2023-01-05 16:02:18 +0300

    add darkmode

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,2 +1,3 @@
 import './components/_nav-toggle';
 import './components/_back-to-top';
+import './components/_dark-mode';
--- src/js/components/_nav-height.js
+++
@@ -1,2 +0,0 @@
-let navList = document.querySelector('.nav-list');
-//navList.getAttribute('bottom', 0);
