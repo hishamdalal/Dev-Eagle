4cb5d7e8ec0d2add627d33c279439b4c0ba978f3
Hisham Dalal <hishamdalal@gmail.com>
2023-03-13 22:39:02 +0300

    hide nav-list on press Escape

--- src/js/components/_NavToggle.js
+++ src/js/components/_NavToggle.js
@@ -10,13 +10,13 @@ export function NavToggle_Run (){
           navList.setAttribute('data-visible', true);
           navToggle.setAttribute('aria-expanded', true);
           navToggle.classList.add('show');
-          body_overflow.style.overflow = "hidden";
+          body_overflow.style.overflowY = "hidden";
       }
       else if ( visibility === "true" ) {
           navList.setAttribute('data-visible', false);
           navToggle.setAttribute('aria-expanded', false);
           navToggle.classList.remove('show');
-          body_overflow.style.overflow = "auto";
+          body_overflow.style.overflowY = "auto";
       }
   });
 
@@ -26,10 +26,23 @@ export function NavToggle_Run (){
       navList.setAttribute('data-visible', false);
       navToggle.setAttribute('aria-expanded', false);
       navToggle.classList.remove('show');
-      body_overflow.style.overflow = "auto";
+      body_overflow.style.overflowY = "auto";
     }
   });
 
+  // hide .nav-list when press escape
+  document.addEventListener("keyup", (e) => {
+    // if nav-list is visible hide it using navToggle click trigger
+    if(e.key == 'Escape' && navList.getAttribute('data-visible') == 'true') {
+      
+      // https://stackoverflow.com/a/6367376/2269902  
+      if (navToggle.onclick) {
+          navToggle.onclick();
+        } else if (navToggle.click) {
+          navToggle.click();
+        }
+    }
+  })
 
   // https://stackoverflow.com/a/1147768/2269902
   // Make .nav-list height = body height
